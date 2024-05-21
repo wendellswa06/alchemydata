b89037750ddcad69cee2673aba158273f978313f
wendellswa06 <wthompsonswa06@gmail.com>
2024-05-21 14:34:26 -0400

    create converter

--- prepare_caption.py
+++ prepare_caption.py
@@ -1,4 +1,49 @@
 import json
+from openai import OpenAI
+
+def converter(inputfile: str):
+    text = "A very clean and well decorated empty bathroom A blue and white bathroom with butterfly themed wall tiles. A bathroom with a border of butterflies and blue paint on the walls above it. An angled view of a beautifully decorated bathroom. A clock that blends in with the wall hangs in a bathroom."
+        
+    openai_client = OpenAI(api_key="")
+    model = "gpt-4"
+    
+    with open(inputfile, 'r') as f:
+        lines = f.readlines()
+
+    # Create a dictionary to store the merged texts
+    converted_texts = {}
+
+    # Iterate through the lines and merge the texts
+    for i, line in enumerate(lines):
+        print(f"NOTE: {i}")
+        data = json.loads(line.strip())
+        file_name = data['file_name']
+        text = data['text']
+        prompt = f"You are an image prompt generator. Your purpose is to convert {text} into a single one sentence prompt that can be fed into Dalle-3."
+        response = openai_client.chat.completions.create(
+            model=model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": prompt,
+                },
+            ],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        response = response.choices[0].message.content    
+        converted_texts[file_name] = response
+    # Write the merged texts to a new file
+    with open("converted"+inputfile, 'w') as f:
+        for file_name, text in converted_texts.items():
+            f.write(f'{{"file_name": "{file_name}", "text": "{text}"}}\n')
+    
+    
+    
 def merge(inputfile: str):
     # Read the contents of the A.txt file
     with open(inputfile, 'r') as f:
@@ -23,4 +68,5 @@ def merge(inputfile: str):
         for file_name, text in merged_texts.items():
             f.write(f'{{"file_name": "{file_name}", "text": "{text}"}}\n')
 if __name__ == '__main__':
-    merge("metadata.jsonl")
+    # merge("metadata.jsonl")
+    converter("test_merged.txt")
